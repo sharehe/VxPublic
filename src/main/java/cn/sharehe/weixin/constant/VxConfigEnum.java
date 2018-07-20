eb54a5468bd747664844c54c2a25ec200574f866
xiaoxiaowugui <245043894@qq.com>
2018-07-20 15:25:36 +0800

    '添加README'‘

--- src/main/java/cn/sharehe/weixin/constant/VxConfigEnum.java
+++ src/main/java/cn/sharehe/weixin/constant/VxConfigEnum.java
@@ -6,10 +6,8 @@ package cn.sharehe.weixin.constant;
  * @Date 2018-7-17 11:12
  */
 public enum VxConfigEnum {
-    // wxc99b26be46e8a086
-    APPID("wxc99b26be46e8a086"),// wx1e30b24a6dec285e
-    //c2680a6106b11c0bd8e8dfc1a4b90b1a
-    APPSECRET("c2680a6106b11c0bd8e8dfc1a4b90b1a"),  //047248ba7ee006550f992a456897a08c
+    APPID("wxc99b26be46e8a086"),//
+    APPSECRET("c2680a6106b11c0bd8e8dfc1a4b90b1a"),  //
     ROOTURLS("https://api.weixin.qq.com/"),
     ROOTURL("http://api.weixin.qq.com/"),
     MYTOKEN("wugui");   // 验证token
--- src/main/java/cn/sharehe/weixin/vxinterface/GetToken.java
+++ src/main/java/cn/sharehe/weixin/vxinterface/GetToken.java
@@ -10,7 +10,7 @@ import com.alibaba.fastjson.JSONObject;
  * @Date 2018-7-17 11:15
  */
 public class GetToken {
-    private static String Token = "11_Xm69QnAd6TxWBzErvoNYWdTe-1LrVUPF2qw2vbU85pKIktqhG9kyQqyOLjnmxRZcwUTKgdMkdMeNgORYoXQRZ5KkyS_9tLr3VtsBBtHde_64-CGGghocZ_51woR-aSR4pRK6Ekqw1NXXkJanOFMeAGAGXE";
+    private static String Token = null;
     public static String getToken(){
         if (Token != null)
             return Token;
